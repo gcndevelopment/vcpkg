4f5155e7fc5560c78737b68ac745a404efb4206a
gnieboer <gnieboer@corpcomm.net>
2021-01-03 14:07:33 -0500

    [glib] updated to 2.58.0 and switch to meson build system

--- ports/glib/portfile.cmake
+++ ports/glib/portfile.cmake
@@ -6,28 +6,27 @@ if (VCPKG_TARGET_IS_WINDOWS)
     vcpkg_check_linkage(ONLY_DYNAMIC_LIBRARY)
 endif()
 
-set(GLIB_VERSION 2.52.3)
+set(GLIB_VERSION 2.67.1)
 vcpkg_download_distfile(ARCHIVE
-    URLS "https://ftp.gnome.org/pub/gnome/sources/glib/2.52/glib-${GLIB_VERSION}.tar.xz"
+    URLS "https://ftp.gnome.org/pub/gnome/sources/glib/2.67/glib-${GLIB_VERSION}.tar.xz"
     FILENAME "glib-${GLIB_VERSION}.tar.xz"
-    SHA512 a068f2519cfb82de8d4b7f004e7c1f15e841cad4046430a83b02b359d011e0c4077cdff447a1687ed7c68f1a11b4cf66b9ed9fc23ab5f0c7c6be84eb0ddc3017)
+    SHA512 f7a576ab454633e6ad6ea00153a57f2f9d6bb20ec2c200fad43fe743192e3b912a7d4774e8c99594e3c53fa32853741c32a7df6c396226ea18c822e872c006dc)
 
 vcpkg_extract_source_archive_ex(
     OUT_SOURCE_PATH SOURCE_PATH
     ARCHIVE ${ARCHIVE}
     REF ${GLIB_VERSION}
     PATCHES
-        use-libiconv-on-windows.patch
+#        use-libiconv-on-windows.patch
         arm64-defines.patch
-        fix-arm-builds.patch
-        fix_pkgconfig.patch
+		meson.build.patch
 )
 
 configure_file(${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt  ${SOURCE_PATH}/CMakeLists.txt COPYONLY)
 file(COPY ${CMAKE_CURRENT_LIST_DIR}/cmake DESTINATION ${SOURCE_PATH})
 file(REMOVE_RECURSE ${SOURCE_PATH}/glib/pcre)
 file(WRITE ${SOURCE_PATH}/glib/pcre/Makefile.in)
-file(REMOVE ${SOURCE_PATH}/glib/win_iconv.c)
+#file(REMOVE ${SOURCE_PATH}/glib/win_iconv.c)
 
 if (selinux IN_LIST FEATURES AND NOT VCPKG_TARGET_IS_WINDOWS AND NOT EXISTS "/usr/include/selinux")
     message("Selinux was not found in its typical system location. Your build may fail. You can install Selinux with \"apt-get install selinux\".")
@@ -36,25 +35,32 @@ endif()
 vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
     selinux HAVE_SELINUX
 )
-
-vcpkg_configure_cmake(
-    SOURCE_PATH ${SOURCE_PATH}
-    PREFER_NINJA
-    OPTIONS ${FEATURE_OPTIONS}
-        -DGLIB_VERSION=${GLIB_VERSION}
-    OPTIONS_DEBUG
-        -DGLIB_SKIP_HEADERS=ON
-        -DGLIB_SKIP_TOOLS=ON
-)
-
-vcpkg_install_cmake()
-vcpkg_fixup_cmake_targets(CONFIG_PATH share/unofficial-glib TARGET_PATH share/unofficial-glib)
+if (VCPKG_TARGET_IS_WINDOWS OR VCPKG_TARGET_IS_OSX)
+	vcpkg_configure_meson(
+		SOURCE_PATH ${SOURCE_PATH}
+		OPTIONS
+        -DG_OS_WIN32=ON
+		-Dinstalled_tests=false
+	)
+	vcpkg_install_meson()
+endif() 
 
 vcpkg_copy_pdbs()
+file(GLOB EXEFILES_RELEASE ${CURRENT_PACKAGES_DIR}/bin/*.exe)
+file(GLOB EXEFILES_DEBUG ${CURRENT_PACKAGES_DIR}/debug/bin/*.exe)
+file(COPY ${EXEFILES_RELEASE} DESTINATION ${CURRENT_PACKAGES_DIR}/tools/${PORT})
+file(REMOVE ${EXEFILES_RELEASE} ${EXEFILES_DEBUG})
 vcpkg_copy_tool_dependencies(${CURRENT_PACKAGES_DIR}/tools/${PORT})
 
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/share)
+
+configure_file(
+    ${SOURCE_PATH}/cmake/unofficial-glib-config.in.cmake
+    ${CMAKE_CURRENT_BINARY_DIR}/cmake/unofficial-glib-config.cmake
+    @ONLY
+)
 
 vcpkg_fixup_pkgconfig()
-
+file(INSTALL ${CMAKE_CURRENT_BINARY_DIR}/cmake/unofficial-glib-config.cmake DESTINATION ${CURRENT_PACKAGES_DIR}/share/unofficial-glib)
 file(INSTALL ${SOURCE_PATH}/COPYING DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
